2f4a35788bf58e85f69c99bc0663d283a6352a14
DerekZhang <DerekZhang@192.168.0.28>
2016-10-14 17:41:01 -0400

    db change

--- application/rivulet_db.sql
+++ application/rivulet_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `status` int(10) unsigned zerofill NOT NULL COMMENT '0 - default init\n1 - actived\n2 - expired',
+  `status` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '0 - default init1 - actived2 - expired',
   `ip` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -15,8 +15,8 @@ CREATE TABLE `user_signup` (
   `signup_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `status` int(10) unsigned zerofill NOT NULL COMMENT '0 - default init\n1 - actived\n2 - expired',
+  `status` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '0 - default init1 - actived2 - expired',
   `activate_url` varchar(255) NOT NULL,
   `ip` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`signup_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
